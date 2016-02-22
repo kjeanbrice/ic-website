5d1273269088e7e6f2d949f7ed2c526ee51eb196
Karl Jean-Brice <kjeanbri@hotmail.com>
2016-02-22 14:58:22 -0500

    Added comments to index.html for readability Added "ibm-builder.js"  to help with form validation. Added "count-password-fields.js" for Assignment2b. Corrected margins and styles in "layout.css".

---
+++ public_html/scripts/count-password-fields.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+function countPasswordFields(){
+        
+        /*
+        var doc = document.getElementsByTagName("input");
+        var count = 0;
+        for(var i = 0; i<doc.length; i++)
+        {
+           if(doc[i].getAttribute("type") === "password")
+           {
+                count = count + 1;
+           }
+        } 
+        alert("Password form fields: " + count);*/
+}
+
